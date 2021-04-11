0ad838095b2121b01dc6da134c0fbc29ff76fdf4
xyqlx <mxxyqlx@outlook.com>
2021-04-11 15:36:53 +0800

    globalization

--- ChocolaTimer/App.xaml.cs
+++ ChocolaTimer/App.xaml.cs
@@ -13,5 +13,16 @@ namespace ChocolaTimer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var culture = System.Globalization.CultureInfo.CurrentCulture;
+            if(culture.TwoLetterISOLanguageName == "zh")
+            {
+                var langResource = LoadComponent(new Uri(@"lang\Chinese.xaml", UriKind.Relative)) as ResourceDictionary;
+                this.Resources.MergedDictionaries.Clear();
+                this.Resources.MergedDictionaries.Add(langResource);
+            }
+            base.OnStartup(e);
+        }
     }
 }
